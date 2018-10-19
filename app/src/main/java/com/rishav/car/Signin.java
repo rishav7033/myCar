ac84e3d31a50f62a88ff11998b371a286f545d03
Chirag Rawal <hivethehacker@gmail.com>
2018-10-19 13:03:01 +0530

    Sign Up Screen Added - HackrHIVE

--- app/src/main/java/com/rishav/car/Signin.java
+++ app/src/main/java/com/rishav/car/Signin.java
@@ -1,13 +1,38 @@
 package com.rishav.car;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Signin extends AppCompatActivity {
 
+    EditText userName,userEmail,userPhone,userDOB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+
+    public void CreateAccount(View view) {
+        userName = findViewById(R.id.nameEdit);
+        userEmail = findViewById(R.id.emailEdit);
+        userPhone = findViewById(R.id.phoneEdit);
+        userDOB = findViewById(R.id.dobEdit);
+
+        String name = userName.getText().toString();
+        String phone = userPhone.getText().toString();
+        String email = userEmail.getText().toString();
+        String DOB = userDOB.getText().toString();
+
+
+        // Store these details in Local Storage
+        // --> Code for Storage
+
+
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
